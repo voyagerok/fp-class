391f9e192cd4e027c47bfd5b48678e1f9bff2789
voyagerok <shepard1010@gmail.com>
2014-10-27 21:39:47 +0400

    1 в 7

--- class-07/01-genintfile.hs
+++ class-07/01-genintfile.hs
@@ -7,4 +7,28 @@
   4) количество строк в файле.
 -}
 
-main = undefined
+import System.IO
+import System.Environment
+import Control.Monad
+import Data.List
+import Data.Char
+import System.Directory
+import System.Random
+
+appF fname n m minN maxN =
+	if (m > 0) then do
+		gen1 <- newStdGen
+		let list = take n $  (randomRs (minN, maxN) gen1 :: [Int])
+		appendFile fname $ (foldl (\acc x -> acc++(show x)++" ") [] list) ++ "\n"
+		appF fname n (m - 1) minN maxN
+	else
+		return ()
+
+genFile (fname : minN : maxN : maxL : maxS : []) = do
+	exists <- doesFileExist fname
+	when (exists) $ removeFile fname
+	appF fname (read maxS) (read maxL) (read minN) (read maxN)
+
+main = do
+args <- getArgs
+genFile args
