520356a3247c802fab38546f7debda06ef377331
voyagerok <shepard1010@gmail.com>
2014-11-19 04:29:10 +0400

    1 в 10

--- class-10/01-data-reader.hs
+++ class-10/01-data-reader.hs
@@ -9,4 +9,26 @@
    блока do не допускается).
 -}
 
-main = undefined
+import Control.Monad
+import Data.Ord
+import Data.List
+
+data Student = Student {name :: String, age :: Int, group :: String} deriving (Eq)
+
+instance Show Student where
+  show (Student n a g) = n ++ " " ++ (show a) ++ " " ++ g ++ "\n"
+  
+instance Ord Student where
+  compare (Student n1 a1 g1) (Student n2 a2 g2) = compare n1 n2
+  
+readData [] = []
+readData xs = (take 3 xs) : readData (drop 3 xs)
+
+intoStudent xs = foldl (\acc [n, a, g] -> (Student n (read a) g) : acc ) [] xs
+
+readF fname = (return . intoStudent . readData . words) `liftM` (readFile fname)
+
+writeF :: [String] -> IO ()
+writeF st = return (show st) >>= writeFile "st12.txt"
+
+main = ((++) `liftM` (readF "st1.txt") `ap` (readF "st2.txt")) >>= (writeF . concat . sort)
